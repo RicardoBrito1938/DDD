2688df5061d99616e38ba0f6e9f51f8463765fe6
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-08-17 18:35:34 +0100

    chore: new entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "crypto";
 
+interface AnswerProps {
+  content: string;
+  questionId: string;
+  authorId: string;
+}
+
 export class Answer {
   public id: string;
   public content: string;
+  public authorId: string;
+  public questionId: string;
 
-  constructor(content: string, id?: string) {
+  constructor({ authorId, content, questionId }: AnswerProps, id?: string) {
     this.content = content;
     this.id = id ?? randomUUID();
+    this.authorId = authorId;
+    this.questionId = questionId;
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,21 @@
 import { randomUUID } from "crypto";
 
+interface QuestionProps {
+  title: string;
+  content: string;
+  authorId: string;
+}
+
 export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public authorId: string;
 
-  constructor(title: string, content: string, id?: string) {
+  constructor({ authorId, content, title }: QuestionProps, id?: string) {
     this.title = title;
     this.content = content;
     this.id = id ?? randomUUID();
+    this.authorId = authorId;
   }
 }
