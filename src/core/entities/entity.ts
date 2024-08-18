0b238a8a969f3bdea085dae7bf9b6e6707dafd25
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-08-18 10:34:38 +0100

    chore: entity id

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto";
+import { UniqueEntityId } from "./unique-entity-id";
 
 export class Entity<T> {
-	private _id: string;
+	private _id: UniqueEntityId;
 	protected props: T;
 
-	get id(): string {
+	get id() {
 		return this._id;
 	}
 
 	constructor(props: T, id?: string) {
 		this.props = props;
-		this._id = id ?? randomUUID();
+		this._id = new UniqueEntityId(id);
 	}
 }
