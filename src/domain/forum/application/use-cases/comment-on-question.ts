549281f614afd5cb9df2f719d5be97a2614beb67
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-11-28 14:55:17 +0000

    chore: comment in a question

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,46 @@
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import type { QuestionsRepository } from "../repositories/questions-repository";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { QuestionComment } from "../../enterprise/entities/question-comment";
+import type { QuestionCommentsRepository } from "../repositories/question-comments-repository";
+
+interface CommentOnQuestionUseCaseRequest {
+	authorId: string;
+	questionId: string;
+	content: string;
+}
+
+interface CommentOnQuestionUseCaseResponse {
+	questionComment: QuestionComment;
+}
+
+export class CommentOnQuestionUseCase {
+	constructor(
+		private questionsRepository: QuestionsRepository,
+		private questionCommentsRepository: QuestionCommentsRepository,
+	) {}
+
+	async execute({
+		authorId,
+		questionId,
+		content,
+	}: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+		const question = await this.questionsRepository.findById(questionId);
+
+		if (!question) {
+			throw new Error("Question not found");
+		}
+
+		const questionComment = QuestionComment.create({
+			authorId: new UniqueEntityId(authorId),
+			content,
+			questionId: new UniqueEntityId(questionId),
+		});
+
+		await this.questionCommentsRepository.create(questionComment);
+
+		return {
+			questionComment,
+		};
+	}
+}
