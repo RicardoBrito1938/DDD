aeffbcdc7385441360944e156cf9edcc99c57949
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-11-28 13:40:56 +0000

    chore: base class for comments

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,39 +1,14 @@
-import { Entity } from "@/core/entities/entity";
 import type { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import type { Optional } from "@/core/types/optional";
+import { Comment, type CommentProps } from "./comment";
 
-export interface QuestionCommentProps {
-	authorId: UniqueEntityId;
+export interface QuestionCommentProps extends CommentProps {
 	questionId: UniqueEntityId;
-	content: string;
-	createdAt: Date;
-	updatedAt?: Date;
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-	get content(): string {
-		return this.props.content;
-	}
-
-	get authorId(): UniqueEntityId {
-		return this.props.authorId;
-	}
-
-	get createdAt(): Date {
-		return this.props.createdAt;
-	}
-
-	get updatedAt(): Date | undefined {
-		return this.props.updatedAt;
-	}
-
-	private touch() {
-		this.props.updatedAt = new Date();
-	}
-
-	set content(content: string) {
-		this.props.content = content;
-		this.touch();
+export class QuestionComment extends Comment<QuestionCommentProps> {
+	get questionId() {
+		return this.props.questionId;
 	}
 
 	static create(
