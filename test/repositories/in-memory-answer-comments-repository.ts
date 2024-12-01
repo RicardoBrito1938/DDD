38e55bda64fa49a70cdaa2f17ffd64eec538a975
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-12-01 09:03:28 +0000

    chore: delete answer comment

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -10,4 +10,14 @@ export class InMemoryAnswerCommentsRepository
 		this.items.push(answerComment);
 		return Promise.resolve();
 	}
+
+	async findById(id: string): Promise<AnswerComment | null> {
+		const answerComment = this.items.find((item) => item.id.toString() === id);
+		return Promise.resolve(answerComment || null);
+	}
+
+	async delete(answerComment: AnswerComment): Promise<void> {
+		this.items = this.items.filter((item) => item.id !== answerComment.id);
+		return Promise.resolve();
+	}
 }
